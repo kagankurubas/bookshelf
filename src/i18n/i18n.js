a69597f3477cf0d0ef7a3007b69aa3dac6a57d9f
Nuri Kağan Kurubaş <38941644+kagankurubas@users.noreply.github.com>
2026-07-29 16:22:04 +0300

    feat: integrate i18n multi-language support (EN/TR)

---
+++ src/i18n/i18n.js
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import trTranslation from './locales/tr.json';
+import enTranslation from './locales/en.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      tr: { translation: trTranslation }
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
